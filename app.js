67a9f40bbf2e81ab90ccf2136e70d174d506bffb
Prabod Ashan <prabodashan14@gmail.com>
2022-09-16 11:00:53 +0530

    Added database connection with mongoDB

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-console.log("Hello world");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { ErrorHandler } = require("./Middleware/ErrorMiddleware");
 
+const connectDB = require("./Config/Db");
+connectDB();
+
+const port = process.env.PORT || 3300;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use("/api/notes", require("./Routes/NoteRoutes"));
+
+// app.use(ErrorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
